d5154597a161bb2f778acabb28bcfe53edb55ee0
Juan Pablo <uncorreodeinternet@gmail.com>
2020-11-14 20:15:45 -0500

    [#7] Added JSON decoder function on resource

--- pkg/base_resource.go
+++ pkg/base_resource.go
@@ -1,13 +1,24 @@
 package resources
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func Get(path string) (*http.Response, error) {
-
 	log.Printf("Making a GET request to %v", path)
 
-	return http.Get(path)
+	response, err := http.Get(path)
+	if err != nil {
+		log.Fatalf("Error maiking the request %v", err)
+	}
+
+	return response, err
+}
+
+func ToStruct(response http.Response, target interface{}) error {
+	defer response.Body.Close()
+
+	return json.NewDecoder(response.Body).Decode(&target)
 }

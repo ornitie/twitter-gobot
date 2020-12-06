8d365fc4c9d001bc27c0b83a3857fe14730b8d08
Juan Pablo <uncorreodeinternet@gmail.com>
2020-12-05 20:25:23 -0500

    [#12] Added Feature to create new rule

--- cmd/services/rules.go
+++ cmd/services/rules.go
@@ -1,6 +1,14 @@
 package services
 
-import "github.com/ornitie/twitter-gobot/pkg"
+import (
+	"github.com/ornitie/twitter-gobot/cmd/models"
+	"github.com/ornitie/twitter-gobot/pkg"
+)
+
+const (
+	BASE_URL   = "https://api.twitter.com/2/"
+	STREAM_URI = "tweets/search/stream/"
+)
 
 type (
 	RulesService struct {
@@ -10,4 +18,12 @@ type (
 
 func NewRulesService(baseResource resources.BaseResource) *RulesService {
 	return &RulesService{baseResource: baseResource}
+}
+
+func (service RulesService) CreateNewRule(ruleValue string) error {
+	createRule := models.CreateRule{Add: []models.Rule{models.Rule{Value: ruleValue}}}
+
+	_, err := service.baseResource.Post(BASE_URL+STREAM_URI+"rules", createRule)
+
+	return err
 }

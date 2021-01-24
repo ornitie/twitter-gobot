f3336652ea9f9248bee54f5f25471b1254df60ba
Juan Pablo <uncorreodeinternet@gmail.com>
2021-01-23 19:27:26 -0500

    [#41] Added status modifier for router when error on request happen

--- cmd/server/routes.go
+++ cmd/server/routes.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 )
 
-type handler func(*http.Request) interface{}
+type handler func(*http.Request) (interface{}, error)
 
 func mapRoutes(api *api) error {
 	router := mux.NewRouter()
@@ -23,13 +23,18 @@ func mapRoutes(api *api) error {
 
 func (api *api) genericRouter(function handler) func(w http.ResponseWriter, r *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		response := function(request)
+		response, error := function(request)
 
 		writer.Header().Set("Content-Type", "application/json")
-		error := json.NewEncoder(writer).Encode(response)
 
 		if error != nil {
-			log.Printf("Error response %v", error)
+			writer.WriteHeader(http.StatusBadRequest)
+		}
+
+		encoding_error := json.NewEncoder(writer).Encode(response)
+
+		if encoding_error != nil {
+			log.Printf("Error response %v", encoding_error)
 		}
 	}
 }
